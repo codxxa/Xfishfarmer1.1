a39d6dfe0574d305610ac3bcb689775af20c7f9e
codxxa <79031539+codxxa@users.noreply.github.com>
2025-03-23 21:11:44 +0200

    changes

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from sqlalchemy.orm import DeclarativeBase
 from werkzeug.utils import secure_filename
 
 
-from flask_migrate import Migrate
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'qlite:///xfishfarmer.db'  # Replace with your database URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
